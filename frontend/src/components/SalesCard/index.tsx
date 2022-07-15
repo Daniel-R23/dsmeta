5ddd4e37285618c8eeb6f84deafec5914512a3b4
Daniel-R23 <danielima1023@gmail.com>
2022-07-15 18:39:14 -0300

    Correção nome arquivo Sale

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
-import { sale } from "../../models/Sale"
+import { sale } from "../../models/sale"
 import { BASE_URL } from "../../utils/request"
 import NotificationButton from '../NotificationButton'
 import './styles.css'
